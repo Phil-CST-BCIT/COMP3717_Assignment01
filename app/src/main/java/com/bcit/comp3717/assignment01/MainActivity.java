b448ec92571dca464494e84252dfe6c3ea1c9727
philteng <philfeiteng@gmail.com>
2020-10-16 19:05:10 -0700

    init commit

---
+++ app/src/main/java/com/bcit/comp3717/assignment01/MainActivity.java
@@ -0,0 +1,12 @@
+package com.bcit.comp3717.assignment01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
